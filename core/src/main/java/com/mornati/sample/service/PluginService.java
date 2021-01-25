8b5f5adfe13bb761f957277b3bd548e9e8cda77e
Jürgen Albert <j.albert@data-in-motion.biz>
2021-01-25 09:48:05 +0100

    Fix: hopefully solves slf4j issue
    Signed-off-by: Jürgen Albert <j.albert@data-in-motion.biz>

--- core/src/main/java/com/mornati/sample/service/PluginService.java
+++ core/src/main/java/com/mornati/sample/service/PluginService.java
@@ -8,9 +8,14 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.StringJoiner;
+
 import javax.annotation.PreDestroy;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -44,6 +49,10 @@ public class PluginService {
     try {
       // Create an instance and initialise the framework.
       FrameworkFactory factory = new org.apache.felix.framework.FrameworkFactory();
+      
+      Map<String, String> felixProperties = new HashMap<>(felixConfiguration.getPluginsService());
+      felixProperties.put("org.osgi.framework.system.packages.extra", findPackageNamesStartingWith());
+      
       framework = factory.newFramework(felixConfiguration.getPluginsService());
       framework.init();
 
@@ -67,6 +76,23 @@ public class PluginService {
     }
   }
 
+  /**
+   * Felix provides an isolated classloader to each bundle. Bundles need to declare what packages they need in there manifest.
+   * If a Bundle needs e.g. something from orgl.slf4j it aither needs to have it as classes itself or another bundle must export this packages.
+   * java.* packages will be provided by the framework from the classloader it was created with. We can modify this, by giving the framework
+   * a list of additional packages, that are provided by the spring boot container (should include e.g.slf4j)
+  * @return
+  */
+  public String findPackageNamesStartingWith() {
+	  StringJoiner joiner = new StringJoiner(",");  
+	  Arrays.asList(Package.getPackages()).stream()
+	        .map(Package::getName)
+	        .filter(n -> !n.startsWith("java")) // the framework will expose this automatically
+	        .filter(n -> !n.startsWith("org.apache.felix")) // we don't want the inner workings of the felix framework exposed
+	        .forEach(joiner::add);
+	  return joiner.toString();
+	}
+  
   @PreDestroy
   public void destroy() throws BundleException, InterruptedException {
     log.info("Stopping plugins OSGi service...");
